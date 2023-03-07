d75c0ed19099ef5d6793ae8a5d936d7c9371e5dd
shoet <32211937+shoet@users.noreply.github.com>
2023-03-07 21:27:07 +0900

    DBマイグレーション

--- apps/app.py
+++ apps/app.py
@@ -5,11 +5,10 @@ from flask import (
     Flask,
 )
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_debugtoolbar import DebugToolbarExtension
 
 from apps.config import config
-from apps.admin import views as admin_views
-from apps.blog import views as blog_views
 
 base_dir = Path(__file__).parent.parent
 if not os.path.exists(Path(base_dir, '.env')):
@@ -25,9 +24,13 @@ def create_app(env):
     app.config.from_object(config[env])
 
     db.init_app(app)
+    Migrate(app, db)
     debug_toolbar.init_app(app)
 
+    from apps.admin import views as admin_views
     app.register_blueprint(admin_views.admin_app, url_prefix='/admin')
+
+    from apps.blog import views as blog_views
     app.register_blueprint(blog_views.blog_app, url_prefix='/blog') # TODO: 変える
 
     # TODO: 404, 500
--- apps/config.py
+++ apps/config.py
@@ -1,12 +1,21 @@
 import os
 
+
 class BaseConfig():
     SECRET_KEY = os.environ['SECRET_KEY']
     DEBUG_TB_INTERCEPT_REDIRECTS = False
 
 
 class DevConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI=(
+        f'postgresql://{os.environ["PG_HOST_DEV"]}:' 
+        + f'{os.environ["PG_PORT_DEV"]}/'
+        + f'{os.environ["PG_DB_DEV"]}?' 
+        + f'user={os.environ["PG_USER_DEV"]}&'
+        + f'password={os.environ["PG_PASSWORD_DEV"]}&'
+        + f'options=-c%20search_path={os.environ["PG_SCHEMA_DEV"]}')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    SQLALCHEMY_ECHO=True
 
 config = {
     'dev': DevConfig,
